58a4c625e28a05d004885983a5ecb1c54917816e
mastio1836 <prasetyotio775@gmail.com>
2019-11-11 23:26:52 -0800

    Gambar

--- latihan2.py
+++ latihan2.py
@@ -1,9 +1,10 @@
-x = int()                                       ## Memperkenalkan variablr
-y = 0
-while x >= 0:
-    x = int(input("Masukan Bilangan: "))
-    if x > y:
-     y = int(x)
-    if x == 0:
-     break
-print("\nAngka Terbesar Adalah ",y)
+x = int()                                       ## Memperkenalkan variable x sebagai integer, kemudian menginputkan nilainya
+y = 0                                           ## Memperkenalkan variable y dengan nilai 0
+while x >= 0:                                   ## Looping while apabila nilai x tidak sama dengan 0
+    x = int(input("Masukan Bilangan: "))        ## Program yang akan di looping
+    if x > y:                                   ## if kondisi apabila nilai x lebih besar dari nilai y
+     y = int(x)                                 ## Nilai y sama dengan nilai x
+    if x == 0:                                  ## if kondisi apabila nilai x sama dengan 0
+     break                                      ## Fungsi yang menghentikan operasi dibawahnya  jika suatu kondisi yang di tentukan
+print("\nAngka Terbesar Adalah ",y)             ## Mencetak bilangan terbesar
+
--- program1a.py
+++ program1a.py
@@ -1,21 +1,21 @@
-modal = 10000000
-laba = 0
-untung = 0
-for i in range (1,9,1):
- if(i<3):
+modal = 10000000                                            ## Nilai modal
+laba = 0                                                    ## Nilai laba 0
+untung = 0                                                  ## Nilai gambar
+for i in range (1,9,1):                                     ## Perulangan 1 dengan nilai awal 1, nilai akhir 9 dan step 1
+ if(i<3):                                                   ## Kondisi apabila belum bulan ke-3 laba masih 0%
      laba = 0
      untung = untung + laba
- elif(i<5):
+ elif(i<5):                                                 ## Kondisi apabila belum memasuki bulan ke-5, mendapat laba sebesar
     laba = modal*1/100
     untung = untung + laba
- elif(i<8):
+ elif(i<8):                                                 ## Kondisi apabila belum memasuki bulan ke-8, Mendapat laba sebesar
     laba = modal*5/100
     untung = untung + laba
- else:
+ else:                                                      ## Pada bulan ke-8 laba menurun 2%, sehingga laba bulan ke-8 sebesar
     laba = modal*2/100
     untung = untung + laba
-print("Laba Bulan Ke-",i," Sebesar ",laba)
-print("\nTotal Laba adalah: ",untung)
+print("Laba Bulan Ke-",i," Sebesar ",laba)                  ## Mencetak laba per bulan
+print("\nTotal Laba adalah: ",untung)                       ## Menghitung total laba selama 8 bulan
 
 
 
